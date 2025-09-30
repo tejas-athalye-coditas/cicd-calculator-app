fd377c50cc9486f23fb037555090880d03104c46
tejas-athalye-coditas <tejas.athalye@coditas.com>
2025-09-30 17:58:04 +0530

    basic UI changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Calculator',
+  description: 'Created to demonstrate CI/CD',
+  generator: 'simple-calculator',
 }
 
 export default function RootLayout({
