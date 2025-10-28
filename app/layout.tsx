cf82f913ca3a95ec1826d3b2a82959f54461cc30
Tejas Athalye <tejas.athalye@coditas.com>
2025-10-28 15:32:38 +0530

    checking dev branch builds

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'DEV Calculator',
+  title: 'DEV branch Calculator',
   description: 'Created to demonstrate CI/CD',
   generator: 'it-is-simple-calculator-devbranch',
 }
