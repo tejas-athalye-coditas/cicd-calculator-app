d38c08b3fb3889181e63c5f38f59ed4d1a886251
Tejas Athalye <tejas.athalye@coditas.com>
2025-10-28 17:17:35 +0530

    Unique main

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Calculator',
+  title: 'Main branch Calculator',
   description: 'Created to demonstrate CI/CD',
   generator: 'it-is-a-simple-calculator',
 }
